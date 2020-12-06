38b464104b1050f1e11c08a964d06c339928b7ff
DamianAlexanderek <aleksanderek.damian@gmail.com>
2020-12-06 09:55:13 +0100

    add new offers

--- CardForSopot/urls.py
+++ CardForSopot/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('offer_form/', include('offer_form.urls', namespace='offers'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
